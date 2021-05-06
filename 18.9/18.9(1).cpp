31a2f618314b10e60044b2a61c3fdd345aad24ec
Dasha138 <74371426+Dasha138@users.noreply.github.com>
2021-05-06 15:17:05 +0500

    Add files via upload

---
+++ 18.9/18.9(1).cpp
@@ -0,0 +1,27 @@
+﻿#include "Plenty.h"
+#include <iostream> 
+using namespace std;
+int main()
+{
+	try
+	{
+		Plenty x(2);
+		Plenty y;
+		cout << x;
+		cout << "Номер?";
+		int i; cin >> i;
+		cout << x[i] << endl;
+		y = x + 3;
+		cout << y;
+		--x;
+		cout << x;
+		--x;
+		cout << x;
+		--x;
+	}
+	catch (int)
+	{
+		cout << "Ошибка" << endl;
+	}
+	return 0;
+}
